d672175df45662d7f3c91f473bcfe8a19f230918
Wolf Utz <w.utz@psv-neo.de>
2021-05-17 16:37:40 +0200

    Update wrong header and remove test code

--- Classes/Registry/AbstractRegistry.php
+++ Classes/Registry/AbstractRegistry.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the "typo3-graphql" Extension for TYPO3 CMS.
+ * This file is part of the "wpu_graphql" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
@@ -74,5 +74,10 @@ abstract class AbstractRegistry
     public function clear(): void
     {
         $this->items = [];
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
     }
 }
--- Classes/Utility/GraphqlApiManager.php
+++ Classes/Utility/GraphqlApiManager.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the "typo3-graphql" Extension for TYPO3 CMS.
+ * This file is part of the "wpu_graphql" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
